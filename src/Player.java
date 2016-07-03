980d3ebec128b5db193ef695314bd4d2052fb11f
zloilya <zlonast3@gmail.com>
2016-07-03 13:24:16 +0800

    Player

---
+++ src/Player.java
@@ -0,0 +1,20 @@
+
+public class Player {
+
+    private final String name;
+
+    private final String figure;
+
+    public Player(String name, String figure) {
+        this.name = name;
+        this.figure = figure;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFigure() {
+        return figure;
+    }
+}
