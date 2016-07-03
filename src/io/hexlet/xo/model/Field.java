6c547d9c603bca6875aa2420bce9d1aef5694f6e
zloilya <zlonast3@gmail.com>
2016-07-03 22:05:09 +0800

    Good end today

--- src/io/hexlet/xo/model/Field.java
+++ src/io/hexlet/xo/model/Field.java
@@ -5,6 +5,8 @@ import java.awt.*;
 public class Field {
 
     private static final int FIELD_SIZE = 3;
+    private static final int MIN_COORDINATE = 0;
+    private static final int MAX_COORDINATE = FIELD_SIZE;
 
     private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
@@ -17,6 +19,14 @@ public class Field {
 
     public void setFigure(final Point point, final Figure figure){
         field[point.x][point.y] = figure;
+    }
+
+    private boolean checkPoint(final Point point){
+        return chekCoordinate(point.x) && chekCoordinate(point.y);
+    }
+
+    private boolean chekCoordinate(final int coordinate){
+        return coordinate>=MIN_COORDINATE && coordinate<MAX_COORDINATE;
     }
 }
 
