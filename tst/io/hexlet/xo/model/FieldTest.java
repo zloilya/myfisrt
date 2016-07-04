c252ff87c494665e4ee74e072041748552e2fc3b
zloilya <zlonast3@gmail.com>
2016-07-04 15:03:35 +0800

    shit

--- tst/io/hexlet/xo/model/FieldTest.java
+++ tst/io/hexlet/xo/model/FieldTest.java
@@ -1,5 +1,6 @@
 package io.hexlet.xo.model;
 
+import io.hexlet.xo.model.exceptions.AlreadyOccupiedException;
 import io.hexlet.xo.model.exceptions.InvalidPointException;
 import org.junit.Test;
 
@@ -70,6 +71,18 @@ public class FieldTest {
             field.getFigure(inputPoint);
             fail();
         }catch (final InvalidPointException e){}
+    }
+    @Test
+    public void SetFigureWhenAlreadyOccupied() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,0);
+        final Figure inputFigure = Figure.O;
+
+        field.setFigure(inputPoint,inputFigure);
+        try{
+            field.setFigure(inputPoint,inputFigure);
+            fail();
+        }catch (final AlreadyOccupiedException e){}
     }
 }
 
