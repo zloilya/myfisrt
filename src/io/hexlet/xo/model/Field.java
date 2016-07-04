e3bd30682d30b4d341462ee49bb8b05502e9a313
zloilya <zlonast3@gmail.com>
2016-07-04 14:58:38 +0800

    Good start

--- src/io/hexlet/xo/model/Field.java
+++ src/io/hexlet/xo/model/Field.java
@@ -1,6 +1,10 @@
 package io.hexlet.xo.model;
 
+import io.hexlet.xo.model.exceptions.AlreadyOccupiedException;
+import io.hexlet.xo.model.exceptions.InvalidPointException;
+
 import java.awt.*;
+import java.security.InvalidParameterException;
 
 public class Field {
 
@@ -13,11 +17,20 @@ public class Field {
     public int getSize(){
         return FIELD_SIZE;
     }
-    public Figure getFigure(final Point point){
+    public Figure getFigure(final Point point) throws InvalidPointException {
+        if(!checkPoint(point)){
+            throw new InvalidPointException();
+        }
         return field[point.x][point.y];
     }
 
-    public void setFigure(final Point point, final Figure figure){
+    public void setFigure(final Point point, final Figure figure) throws InvalidPointException, AlreadyOccupiedException {
+        if(!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
+        if(field[point.x][point.y]!=null){
+            throw new AlreadyOccupiedException();
+        }
         field[point.x][point.y] = figure;
     }
 
--- tst/io/hexlet/xo/model/FieldTest.java
+++ tst/io/hexlet/xo/model/FieldTest.java
@@ -1,8 +1,10 @@
 package io.hexlet.xo.model;
 
+import io.hexlet.xo.model.exceptions.InvalidPointException;
 import org.junit.Test;
 
 import java.awt.*;
+import java.security.InvalidParameterException;
 
 import static org.junit.Assert.*;
 
@@ -13,7 +15,6 @@ public class FieldTest {
 
         assertEquals(3,field.getSize());
     }
-
     @Test
     public void setFigure() throws Exception {
         final Field field = new Field();
@@ -23,9 +24,53 @@ public class FieldTest {
         field.setFigure(inputPoint,inputFigure);
         final Figure actualFigure = field.getFigure(inputPoint);
 
-        assertEquals(inputFigure,inputFigure);
+        assertEquals(inputFigure,actualFigure);
     }
 
+    @Test
+    public void GetFigureWhenFigureIsNotSet() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,0);
+        final Figure actualFigure = field.getFigure(inputPoint);
+
+        assertNull(actualFigure);
+    }
+    @Test
+    public void GetFigureWhenFigureXIsLessThenZero() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(-1,0);
+        try{
+            field.getFigure(inputPoint);
+            fail();
+        }catch (final InvalidPointException e){}
+    }
+    @Test
+    public void GetFigureWhenFigureYIsLessThenZero() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,-1);
+        try{
+            field.getFigure(inputPoint);
+            fail();
+        }catch (final InvalidPointException e){}
+    }
+    @Test
+    public void GetFigureWhenFigureXIsMoreThenSize() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(field.getSize()+1,0);
+        try{
+            field.getFigure(inputPoint);
+            fail();
+        }catch (final InvalidPointException e){}
+    }
+    @Test
+    public void GetFigureWhenFigureYIsMoreThenSize() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,field.getSize()+1);
+        try{
+            field.getFigure(inputPoint);
+            fail();
+        }catch (final InvalidPointException e){}
+    }
 }
 
 
